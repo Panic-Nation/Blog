56b38ba8038d13ceecc8786248d34436a0e82384
bubbafett5611 <metalgfx@gmail.com>
2025-08-02 21:23:50 +0100

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -88,7 +88,7 @@ const config: Config = {
         //   position: 'left',
         //   label: 'Tutorial',
         // },
-        {to: '/blog', label: 'Blog', position: 'left'},
+        {to: 'blog', label: 'Blog', position: 'left'},
         {
           href: 'https://github.com/Panic-Nation/Blog',
           label: 'GitHub',
